8cb1eab1525b941f2c873cbf80d39c46c9772517
carrelld <dave.carrell@gmail.com>
2016-03-22 16:15:24 -0500

    Making a new window from scratch, didn't get very far

---
+++ src/com/dagrca/scrabble/Game.java
@@ -0,0 +1,22 @@
+package com.dagrca.scrabble;
+
+/**
+ * User: carrelld
+ * Date: 3/22/2016
+ * Time: 3:56 PM
+ * Purpose:
+ */
+public class Game {
+
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
+    }
+
+}
---
+++ src/com/dagrca/scrabble/Window.java
@@ -0,0 +1,40 @@
+package com.dagrca.scrabble;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * User: carrelld
+ * Date: 3/22/2016
+ * Time: 3:57 PM
+ * Purpose:
+ */
+public class Window {
+
+    public static void Window() {
+
+    }
+
+    private static void createAndShowGUI() {
+        JFrame frame = new JFrame("TITLE");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel emptyLabel = new JLabel("");
+        emptyLabel.setPreferredSize(new Dimension(1750, 100));
+        frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+}
